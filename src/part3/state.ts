31af4af33ef88a3bebf8e9ac0db153cdc881424d
Omri Goldberg <goldomri@post.bgu.ac.il>
2021-03-19 18:32:36 +0200

    bind for state completed

--- src/part3/state.ts
+++ src/part3/state.ts
@@ -1,3 +1,8 @@
 export type State<S, A> = (initialState: S) => [S, A];
 
-export const bind = undefined;
+export const bind = <S, A, B>(state: State<S, A>, f: (x: A) => State<S, B>): State<S, B> => {
+    return (initialState: S): [S, B] => {
+        const [s, a] = state(initialState)
+        return f(a)(s)
+    }
+};
