baa2f8f8fec2b9821cdb9ce39d112683d383d395
Omri Goldberg <goldomri@post.bgu.ac.il>
2021-03-20 19:08:25 +0200

    dequeue completed

--- src/part3/queue.ts
+++ src/part3/queue.ts
@@ -6,6 +6,8 @@ export const enqueue = (x: number): State<Queue, undefined> => {
     return initialState => [initialState.concat([x]), undefined];
 };
 
-export const dequeue = undefined;
+export const dequeue: State<Queue, number> = (q: Queue)  => {
+    return [q.slice(1), q[0]];
+};
 
 export const queueManip = undefined;
