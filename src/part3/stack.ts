4a32e11f8bc8b6fd72063375fa6a105e337857ba
Omri Goldberg <goldomri@post.bgu.ac.il>
2021-03-20 19:34:55 +0200

    pop completed

--- src/part3/stack.ts
+++ src/part3/stack.ts
@@ -6,6 +6,8 @@ export const push = (x: number): State<Stack, undefined> => {
     return initialState => [[x].concat(initialState), undefined];
 };
 
-export const pop = undefined;
+export const pop: State<Stack, number> = (s: Stack)  => {
+    return [s.slice(1), s[0]];
+};
 
 export const stackManip = undefined;
