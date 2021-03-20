90654f0ebe25cf84d36ea8005845bce49284abd0
Omri Goldberg <goldomri@post.bgu.ac.il>
2021-03-20 18:58:03 +0200

    enqueue completed

--- src/part3/queue.ts
+++ src/part3/queue.ts
@@ -2,7 +2,9 @@ import { State, bind } from "./state";
 
 export type Queue = number[];
 
-export const enqueue = undefined;
+export const enqueue = (x: number): State<Queue, undefined> => {
+    return initialState => [initialState.concat([x]), undefined];
+};
 
 export const dequeue = undefined;
 
