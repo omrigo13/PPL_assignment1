5fcea3b66bed68188cb90ad19fac6dce8848f163
Omri Goldberg <goldomri@post.bgu.ac.il>
2021-03-20 19:23:08 +0200

    queueManip completed

--- src/part3/queue.ts
+++ src/part3/queue.ts
@@ -10,4 +10,5 @@ export const dequeue: State<Queue, number> = (q: Queue)  => {
     return [q.slice(1), q[0]];
 };
 
-export const queueManip = undefined;
+export const queueManip: State<Queue, number> =
+    bind(dequeue, (x: number) => bind(enqueue(2 * x), (y: undefined) => bind(enqueue(x / 3), (z: undefined) => dequeue)));
